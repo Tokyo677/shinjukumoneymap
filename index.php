ff9972b3f1a13a04b2d13bb1c9885d1d68dab479
Tokyo677 <77230474+Tokyo677@users.noreply.github.com>
2022-01-26 17:22:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     </head>
     <body>
         <br>
-        <form action="#" method="post" style="margin-bottom: 40px">
+        <form action="all.php" method="post" style="margin-bottom: 40px">
             <input type="text" name="shopname" class="input">
         </form>
-        <button onclick="location.href='#'" class="money">D払い</button><br><br>
+        <button onclick="location.href='iD.php'" class="money">iD</button><br><br>
         <button onclick="location.href='PayPay.php'" class="money">PayPay</button><br><br>
         <button onclick="location.href='Rakuten.php'" class="money">楽天Pay</button><br><br>
         <button onclick="location.href='map.html'" class="money">地図</button>
