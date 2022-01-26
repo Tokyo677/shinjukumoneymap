d48002edd3476cbb451d1e3a4a44f66c225be183
Tokyo677 <77230474+Tokyo677@users.noreply.github.com>
2022-01-26 18:22:34 +0900

    Update all.php

--- all.php
+++ all.php
@@ -16,7 +16,6 @@
         $file=fopen('all.csv','rb');
         $Word=$_POST["shopname"];
         while($row=fgetcsv($file,1024,",")) {
-            mb_convert_variables("UTF-8", "SJIS", $row); 
             if(count(preg_grep('/'.$Word.'/', $row)) > 0){
                 print('<tr>');
                 print('<td><h3>'.$row[0].'</h3>'.'<p class="adress">'.$row[1].' , <font color=red>'.$row[2].'</font></p>'.'</td>');
@@ -27,6 +26,6 @@
         ?>
         </table>
     </div>
-        <br><a href="index.html">トップページに戻る</a><br><br>
+        <br><a href="index.php">トップページに戻る</a><br><br>
     </body>
 </html>
