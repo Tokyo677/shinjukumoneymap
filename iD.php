727cf0d2b4ed0f4b1e83e36c98f0961b770e6f23
Tokyo677 <77230474+Tokyo677@users.noreply.github.com>
2022-01-27 22:52:24 +0900

    Update iD.php

--- iD.php
+++ iD.php
@@ -7,14 +7,12 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
     </head>
     <body>
-        <header>
-            <h1>iD対応店 一覧</h1>
-        </header>
-        <br><br>
+        <br>
         <form action="iDSearch.php" method="post" style="margin-bottom: 40px">
-            <input type="text" name="shopname" class="input">
+            <input type="text" name="shopname" class="input" placeholder="検索するテキストを入力">
         </form>
     <div align="center">
+         <h3>iD対応店 一覧</h3>
         <table>
         <?php
         $file=fopen('iD02.csv','rb');
@@ -30,11 +28,11 @@
         <br><a href="index.php">トップページに戻る</a><br><br>
         <footer>
             <ul>
-                <a href="index.php"><li>トップ</li></a>
-                <a href="PayPay.php"><li>PayPay</li></a>
-                <a href="Rakuten.php"><li>楽天Pay</li></a>
-                <a href="iD.php"><li>iD</li></a>
-                <a href="map.html"><li>地図</li></a>
+                <a href="index.php"><li><img class="logo" src="pics/Home2.png"></li></a>
+                <a href="PayPay.php"><li><img class="logo" src="pics/Paypaylogo.png"></li></a>
+                <a href="Rakuten.php"><li><img class="logo" src="pics/Rakutenlogo.png"></li></a>
+                <a href="iD.php"><li><img class="logo" src="pics/iDlogo.png"></li></a>
+                <a href="map.html"><li><img class="logo" src="pics/Map.png"></li></a>
             </ul>
         </footer>
     </body>
