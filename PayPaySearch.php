916916a2f3cc88782e7b878aeb6f1e54a1ddd3bc
Tokyo677 <77230474+Tokyo677@users.noreply.github.com>
2022-01-29 10:49:04 +0900

    Update PayPaySearch.php

--- PayPaySearch.php
+++ PayPaySearch.php
@@ -5,13 +5,15 @@
         <link href="main.css" rel="stylesheet">
         <title>PayPay対応店｜電子マネーMAP</title>
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="icon" type="image/jpg" href="icon.jpg">
+        <link rel="apple-touch-icon" href="icon.jpg">
     </head>
     <body>
     <div align="center">
         <table>
         <?php
         print('<br><form action="PayPaySearch.php" method="post" style="margin-bottom: 40px">');
-        print('<input type="text" name="shopname" class="input" placeholder="検索するテキストを入力">');
+        print('<input type="text" name="shopname" class="input" placeholder="店舗名/住所で検索">');
         print('<br><br><h3 class="pagetitle">'.'PayPay対応店「'.$_POST["shopname"].'」の検索結果'.'</h3>');
         print('</form>');
         $file=fopen('PayPay2.csv','rb');
