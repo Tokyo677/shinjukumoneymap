a9cbdf93ad4df93ffce729555ea93410dfa48a53
Tokyo677 <77230474+Tokyo677@users.noreply.github.com>
2022-01-29 11:13:42 +0900

    Update RakutenSearch.php

--- RakutenSearch.php
+++ RakutenSearch.php
@@ -10,12 +10,12 @@
     </head>
     <body>
     <div align="center">
-        <table>
         <?php
         print('<br><form action="RakutenSearch.php" method="post" style="margin-bottom: 40px">');
         print('<input type="text" name="shopname" class="input" placeholder="店舗名/住所で検索">');
         print('</form>');
         print('<h3 class="pagetitle">'.'楽天Pay対応店「'.$_POST["shopname"].'」の検索結果'.'</h3>');
+        print('<table>');
         $file=fopen('Rakuten1.csv','rb');
         $Word=$_POST["shopname"];
         while($row=fgetcsv($file,1024,",")) {
